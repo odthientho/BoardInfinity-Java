b6f7c985cb0c7d66c2d21c9baceaab237e3c4ca1
Arthur <odthientho@gmail.com>
2024-10-02 22:11:31 -0400

    source codes for Module1 of the course

---
+++ Module1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Reading Name: " + name + ", Age: " + age);
+    }
+}
