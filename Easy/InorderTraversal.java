c3cf4056a01f8646294982800cbde81ce08d17e9
Arthur <odthientho@gmail.com>
2024-11-25 17:01:45 -0500

    inorder traversal practice 94

---
+++ Easy/InorderTraversal.java
@@ -0,0 +1,40 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
+    }
+    public static void main(String[] args) {
+        InorderTraversal inorderTraversal = new InorderTraversal();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        List<Integer> result = inorderTraversal.inorderTraversal(root);
+        System.out.println(result);
+    }
+}
