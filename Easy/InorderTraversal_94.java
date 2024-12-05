135c45cd05b49b56993ceb1a7ba8048a81eeace0
Arthur <odthientho@gmail.com>
2024-12-05 15:20:34 -0500

    one more leetcode practice

--- Easy/InorderTraversal_94.java
+++ Easy/InorderTraversal_94.java
@@ -3,18 +3,6 @@ package Easy;
 import java.util.ArrayList;
 import java.util.List;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 public class InorderTraversal_94 {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
--- Easy/ReverseList_206.java
+++ Easy/ReverseList_206.java
@@ -1,16 +1,5 @@
 package Easy;
 
-
-// Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
-
 public class ReverseList_206 {
 
     public ListNode reverseList(ListNode head) {
